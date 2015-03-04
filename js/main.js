812cf1a10d7501b811ed47f28ae2a484e443257f
acika00mk <aleksandar@kostadinovski.info>
2015-03-05 00:18:15 +0100

    SliderPage functionality

--- js/main.js
+++ js/main.js
@@ -3,12 +3,38 @@ $(function(){
 
 
 
-//    $(".more-site").on('click',function(){
-//        $("#overlay").show();
-//        $('body').append("<img src='../img/thecave_d2.gif' />");
-//
-//    })
+var InfiniteRotator =
+    {
+        init: function()
+        {
+            //initial fade-in time (in milliseconds)
+            var initialFadeIn = 1000;
+            //interval between items (in milliseconds)
+            var itemInterval = 5000;
+            //cross-fade time (in milliseconds)
+            var fadeTime = 2500;
+            //count number of items
+            var numberOfItems = $('.rotatingImage').length;
+            //set current item
+            var currentItem = 0;
+            //show first item
+            $('.rotatingImage').eq(currentItem).fadeIn(initialFadeIn);
+            //loop through the items
+            var infiniteLoop = setInterval(function(){
+                $('.rotatingImage').eq(currentItem).fadeOut(fadeTime);
 
+                if(currentItem == numberOfItems -1){
+                    currentItem = 0;
+                }else{
+                    currentItem++;
+                }
+                $('.rotatingImage').eq(currentItem).fadeIn(fadeTime);
+
+            }, itemInterval);
+        }
+    };
+
+    InfiniteRotator.init();
 
 
 })
