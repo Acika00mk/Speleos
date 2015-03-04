bd310198642e0a72a35530b2e9f7a506e2fbacde
acika00mk <aleksandar@kostadinovski.info>
2015-03-04 21:07:21 +0100

    _

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+        $("#overlay").hide();
+
+
+
+//    $(".more-site").on('click',function(){
+//        $("#overlay").show();
+//        $('body').append("<img src='../img/thecave_d2.gif' />");
+//
+//    })
+
+
+
+})
